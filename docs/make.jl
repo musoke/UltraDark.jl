deedc3681dc964872fa9a067903bbf3aa5692865
Nathan Musoke <nathan.musoke@gmail.com>
2020-11-11 17:07:09 +1300

    ci(doc): Reenable deploying docs in CI

--- docs/make.jl
+++ docs/make.jl
@@ -2,15 +2,17 @@ using Documenter, JultraDark
 
 makedocs(;
     modules=[JultraDark],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://musoke.github.io/JultraDark.jl",
+        assets=String[],
+    ),
     pages=[
         "Home" => "index.md",
     ],
     repo="https://github.com/musoke/JultraDark.jl/blob/{commit}{path}#L{line}",
     sitename="JultraDark.jl",
     authors="Nathan Musoke <n.musoke@auckland.ac.nz>",
-    assets=String[],
-    doctest=false,  # TODO: remove this line
 )
 
 deploydocs(;
