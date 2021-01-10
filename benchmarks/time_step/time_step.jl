e4b68c9f80d888f8c527b023dfb51e4cb65af9e9
Nathan Musoke <nathan.musoke@gmail.com>
2021-01-10 02:37:18 -0400

    fix(bench): Precompile function
    Run the benchmarked function once to make sure it is already compiled.

--- benchmarks/time_step/time_step.jl
+++ benchmarks/time_step/time_step.jl
@@ -20,6 +20,10 @@ grids = Grids(1.0, resol)
 # b = @benchmark take_steps!($grids, $1.0, $Δt, 10, $(OutputConfig("output", 1:2)), $(t -> 1))
 # time_mean = mean(b.times / 1e9 / n_steps)
 
+# Run once to ensure function is precompiled
+take_steps!(grids, 1.0, Δt, n_steps, OutputConfig("output", 1:2), t -> 1)
+
+# Collect data
 res = @timed take_steps!(grids, 1.0, Δt, n_steps, OutputConfig("output", 1:2), t -> 1)
 
 time_mean = mean(res[2] / n_steps)
