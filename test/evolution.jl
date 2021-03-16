4b6c8744249ed86b7ef626a26dcc08a3c70e85fd
Nathan Musoke <nathan.musoke@unh.edu>
2021-03-16 00:24:36 -0300

    test: Run evolution tests on PencilGrids

--- test/evolution.jl
+++ test/evolution.jl
@@ -19,25 +19,29 @@ using MPI
     @test JultraDark.actual_time_step(0.7, 2, 3)[2] == 3
 end
 
-@testset "Take n steps" begin
-    grids = PencilGrids(1.0, 16)
-    output_config = OutputConfig(mktempdir(), [])
-    @test JultraDark.take_steps!(grids, 0, 1, 10, output_config, t -> 1) == 10.0
+for grid_type in [Grids, PencilGrids]
+    @testset "Take n steps, $grid_type" begin
+        grids = grid_type(1.0, 16)
+        output_config = OutputConfig(mktempdir(), [])
+        @test JultraDark.take_steps!(grids, 0, 1, 10, output_config, t -> 1) == 10.0
+    end
 end
 
 # Test that evolve to arrives at right time
-@testset "Evolve from $t_begin to $t_end" for t_begin in [0, 0.123, 1], t_end in [1.23, 2]
-    grids = PencilGrids(1.0, 16)
+for grid_type in [Grids, PencilGrids]
+    @testset "Evolve from $t_begin to $t_end, $grid_type" for t_begin in [0, 0.123, 1], t_end in [1.23, 2]
+        grids = grid_type(1.0, 16)
 
-    a = 1
-    grids.ψx .= grids.dist ./ 1e9 # Set ψx to something non-zero
-    grids.ψk .= (grids.fft_plan * grids.ψx)
-    grids.ρx .= abs2.(grids.ψx)
-    grids.Φk .= -4 * π * (grids.rfft_plan * grids.ρx) ./ (a * grids.rk.^2)
-    grids.Φk[1, 1, 1] = 0
-    grids.Φx .= grids.rfft_plan \ grids.Φk
+        a = 1
+        grids.ψx .= grids.dist ./ 1e9 # Set ψx to something non-zero
+        grids.ψk .= (grids.fft_plan * grids.ψx)
+        grids.ρx .= abs2.(grids.ψx)
+        grids.Φk .= -4 * π * (grids.rfft_plan * grids.ρx) ./ (a * grids.rk.^2)
+        grids.Φk[1, 1, 1] = 0
+        grids.Φx .= grids.rfft_plan \ grids.Φk
 
-    output_config = OutputConfig(mktempdir(), [])
-    options = JultraDark.Config.SimulationConfig(10, t->1)
-    @test JultraDark.evolve_to!(t_begin, t_end, grids, output_config, options) ≈ t_end
+        output_config = OutputConfig(mktempdir(), [])
+        options = JultraDark.Config.SimulationConfig(10, t->1)
+        @test JultraDark.evolve_to!(t_begin, t_end, grids, output_config, options) ≈ t_end
+    end
 end
