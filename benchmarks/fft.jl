371a6914be61b8ca128b74b7201e86fa34fcd929
Nathan Musoke <nathan.musoke@unh.edu>
2021-06-17 15:44:38 -0300

    bench: Add benchmark file for single step
    Add a script to benchmark different ways of doing a single step.
    This makes it clear that something in the implementation of
    `PencilGrids` is slowing it down: a single step with `PencilGrids` is
    much slower than `Grids` and using `PencilArrays` directly.
    
    Related: #37

--- benchmarks/fft.jl
+++ benchmarks/fft.jl
@@ -7,9 +7,7 @@ if ~MPI.Initialized()
     MPI.Init()
 end
 
-n = 64
-
-suite = BenchmarkGroup()
+n = 128
 
 function fft_broadcast!(grids)
     grids.ψk .= grids.fft_plan * grids.ψx
@@ -17,6 +15,10 @@ end
 
 function fft_inplace!(grids)
     mul!(grids.ψk, grids.fft_plan, grids.ψx)
+end
+
+function fft_inplace_sep!(psi_k, psi_x, fft_plan)
+    mul!(psi_k, fft_plan, psi_x)
 end
 
 function fft_inv_broadcast!(grids)
@@ -27,7 +29,12 @@ function fft_inv_inplace!(grids)
     ldiv!(grids.ψx, grids.fft_plan, grids.ψk)
 end
 
+suite = BenchmarkGroup()
+
 for (label, grids) in [("Grids", Grids(1.0, n)), ("PencilGrids", PencilGrids(1.0, n))]
+
+    grids.ψx .= randn(size(grids.ψx))
+    grids.ψk .= randn(size(grids.ψk))
 
     suite[label] = BenchmarkGroup()
 
@@ -36,6 +43,7 @@ for (label, grids) in [("Grids", Grids(1.0, n)), ("PencilGrids", PencilGrids(1.0
     suite[label]["fft_inv_broadcast!"] = @benchmarkable fft_inv_broadcast!($grids)
     suite[label]["fft_inv_inplace!"] = @benchmarkable fft_inv_inplace!($grids)
 
+    suite[label]["fft_inplace_sep!"] = @benchmarkable fft_inplace_sep!($(grids.ψk), $(grids.ψx), $(grids.fft_plan))
 end
 
 @show tune!(suite)
---
+++ benchmarks/fft_basic.jl
@@ -0,0 +1,78 @@
+using BenchmarkTools
+using FFTW
+using LinearAlgebra
+using MPI
+using PencilFFTs
+using Random
+
+function fft_inplace!(a, b, fft_plan)
+    mul!(b, fft_plan, a)
+end
+
+function runtests()
+    suite = BenchmarkGroup()
+
+    if ~MPI.Initialized()
+        MPI.Init()
+    end
+
+    n = 128
+    dims = (n, n, n)
+
+    suite["PencilFFTs"] = BenchmarkGroup()
+    for (flag_name, flags) in [
+                           ("FFTW.ESTIMATE", FFTW.ESTIMATE),
+                           ("FFTW.MEASURE", FFTW.MEASURE),
+                           ("FFTW.PATIENT", FFTW.PATIENT),
+                          ]
+
+        # Distribute 12 processes on a 1 × 1 grid.
+        comm = MPI.COMM_WORLD
+        Nproc = MPI.Comm_size(comm)
+
+        # Let MPI_Dims_create choose the decomposition.
+        proc_dims = let pdims = zeros(Int, 2)
+            MPI.Dims_create!(Nproc, pdims)
+            pdims[1], pdims[2]
+        end
+
+        transform = Transforms.FFT()
+
+        fft_plan = PencilFFTPlan(dims, transform, proc_dims, comm; fftw_flags=flags)
+
+        # Allocate and initialise input data, and apply transform.
+        a_pen = allocate_input(fft_plan)
+        b_pen = allocate_output(fft_plan)
+        randn!(a_pen)
+        randn!(b_pen)
+
+        suite["PencilFFTs"][flag_name] = @benchmarkable fft_inplace!($a_pen, $b_pen, $fft_plan)
+    end
+
+    suite["FFTW"] = BenchmarkGroup()
+    for (flag_name, flags) in [
+                           ("FFTW.ESTIMATE", FFTW.ESTIMATE),
+                           ("FFTW.MEASURE", FFTW.MEASURE),
+                           ("FFTW.PATIENT", FFTW.PATIENT),
+                          ]
+        a_arr = Array{ComplexF64}(undef, dims)
+        b_arr = Array{ComplexF64}(undef, dims)
+        rand!(a_arr)
+        rand!(b_arr)
+
+        fft_plan = plan_fft(a_arr; flags=flags)
+
+        suite["FFTW"][flag_name] = @benchmarkable fft_inplace!($a_arr, $b_arr, $fft_plan)
+    end
+
+    @show tune!(suite)
+
+    results = run(suite)
+
+    @show res_min = minimum(results)
+    @show res_med = median(results)
+
+    @show judge(res_med["FFTW"], res_med["PencilFFTs"])
+end
+
+runtests()
