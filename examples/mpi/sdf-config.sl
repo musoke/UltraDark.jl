956778aa03e3a09420fe7c4cc28bd5fb171e7e79
Nathan Musoke <nathan.musoke@unh.edu>
2021-06-25 15:39:54 -0300

    doc: Refesh moving soliton example
    Combine the two moving soliton examples into one.
    Write some more output.

--- examples/mpi/sdf-config.sl
+++ examples/mpi/sdf-config.sl
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name       "Run an UltraDark test simulation"
 #SBATCH --time           00:10:00   # Walltime
-#SBATCH --ntasks=2
-#SBATCH --cpus-per-task=2
-#SBATCH --mem            4G
+#SBATCH --ntasks=32
+#SBATCH --cpus-per-task=1
+#SBATCH --mem            100G
 #SBATCH --output         log/%j.out
 #SBATCH --error          log/%j.err
 
@@ -14,6 +14,11 @@ module load fftw3/3.3.8-openmpi-4.0.4-gcc-4.8.5
 
 PATH=$PATH:~/julia-1.6.1/bin/
 
+echo "Building MPI.jl" >&2
 julia --project -e 'ENV["JULIA_MPI_BINARY"]="system"; using Pkg; Pkg.build("MPI"; verbose=true)'
 
-mpirun julia --project=. soliton_velocity.jl
+echo "Running simulation" >&2
+export JULIA_DEBUG=UltraDark
+mpirun julia --project ../soliton_velocity.jl
+
+echo "Job done" >&2
--- examples/soliton_velocity.jl
+++ examples/soliton_velocity.jl
@@ -1,78 +1,111 @@
-using UltraDark
+using MPI
 using NPZ
 using Plots
+using UltraDark
+
+if ~MPI.Initialized()
+    MPI.Init()
+end
+comm = MPI.COMM_WORLD
+@info "Process ID $(MPI.Comm_rank(comm)+1) of $(MPI.Comm_size(comm)) with $(Threads.nthreads()) threads\n"
 
 include(joinpath(@__DIR__, "init_soliton.jl"))
 
-output_dir = mktempdir()
-output_dir = "output/vel"
+const resol = 64
 
-num_snapshots = 20
-output_times = LinRange(0, 5, num_snapshots)
+const num_snapshots = 20
+const output_times = LinRange(0, 5, num_snapshots)
 
-output_config = OutputConfig(output_dir, output_times; box=true, slice=false)
-options = Config.SimulationConfig(10)
+const mass = 10
+const position0 = [-4, 0, 0]
+const velocity = [1, 0, 0]
+const phase = 0
+const t0 = 0
 
-mass = 10
-position0 = [-4, 0, 0]
-velocity = [1, 0, 0]
-phase = 0
-t0 = 0
+const output_dir = "output/vel"
 
-resol = 64
-grids = Grids(10.0, resol)
-add_soliton(grids, mass, position0, velocity, phase, t0)
+function run_sim()
 
-simulate(grids, options, output_config) == nothing
+    output_config = OutputConfig(output_dir, output_times; box=true, slice=false)
+    options = Config.SimulationConfig(10)
 
-pos_the = reshape(position0, (1, :)) .+ (reshape(velocity, (1, :)) .* reshape(output_times, (:, 1)))
+    grids = PencilGrids(10.0, resol)
+    @info "Grids created" MPI.Comm_rank(comm) size(grids.ψk)
 
-pos_sim = Array{Float64}(undef, (num_snapshots, 3))
-maxs = Array{Float64}(undef, num_snapshots)
+    mkpath(output_config.directory)
+    npzwrite(joinpath(output_config.directory, "x.npy"), grids.x)
+    npzwrite(joinpath(output_config.directory, "y.npy"), grids.y)
+    npzwrite(joinpath(output_config.directory, "z.npy"), grids.z)
 
-for i in 1:num_snapshots
-    rho = npzread(joinpath(output_dir, "rho_$i.npy"))
+    add_soliton(grids, mass, position0, velocity, phase, t0)
+    @info "Initialized" MPI.Comm_rank(comm)
 
-    rhomax, maxindex = findmax(rho)
-
-    maxs[i] = rhomax
-    pos_sim[i, :] .= [grids.x[maxindex[1]], grids.y[maxindex[2]], grids.z[maxindex[3]]]
+    simulate(grids, options, output_config) == nothing
+    @info "Simulation complete" MPI.Comm_rank(comm)
 
 end
 
-yerr = abs(grids.x[2]-grids.x[1])/2
-plot_pos = plot(legend=:bottomright)
-scatter!(output_times, pos_sim[:, 1], yerr=yerr, markershape=:auto, label="simulation")
-plot!(output_times, pos_the[:, 1], label="theory")
-plot!(xlabel=raw"$t$", ylabel=raw"$x$")
+function plot_results()
+    @info "Starting plots" MPI.Comm_rank(comm)
+    pos_the = reshape(position0, (1, :)) .+ (reshape(velocity, (1, :)) .* reshape(output_times, (:, 1)))
 
-savefig(plot_pos, "soliton_position.pdf")
+    pos_sim = Array{Float64}(undef, (num_snapshots, 3))
+    maxs = Array{Float64}(undef, num_snapshots)
 
-plot_max = plot(legend=:bottomright)
-plot!(output_times, maxs, label="theory")
+    x = npzread(joinpath(output_dir, "x.npy"))
+    y = npzread(joinpath(output_dir, "y.npy"))
+    z = npzread(joinpath(output_dir, "z.npy"))
 
-anim = @animate for i ∈ 1:num_snapshots
-    rho = npzread(joinpath(output_dir, "rho_$i.npy"))
-    psi = npzread(joinpath(output_dir, "psi_$i.npy"))
+    for i in 1:num_snapshots
+        rho = npzread(joinpath(output_dir, "rho_$i.npy"))
 
-    density_plot = contourf(
-                            rho[:, :, resol÷2],
-                            title=raw"$\rho$",
-                            clims=(0, maximum(maxs)),
-                            showaxis=false,
-                            ticks=false,
-                            aspect_ratio=:equal,
-                           )
-    phase_plot = contourf(
-                          angle.(psi[:, :, resol÷2]),
-                          title="phase",
-                          clims=(-π, +π),
-                          showaxis=false,
-                          ticks=false,
-                          aspect_ratio=:equal,
-                         )
+        rhomax, maxindex = findmax(rho)
 
-    plot(density_plot, phase_plot, )
+        maxs[i] = rhomax
+        pos_sim[i, :] .= [x[maxindex[1]], y[maxindex[2]], z[maxindex[3]]]
+
+    end
+
+    yerr = abs(x[2]-x[1])/2
+    plot_pos = plot(legend=:bottomright)
+    scatter!(output_times, pos_sim[:, 1], yerr=yerr, markershape=:auto, label="simulation")
+    plot!(output_times, pos_the[:, 1], label="theory")
+    plot!(xlabel=raw"$t$", ylabel=raw"$x$")
+
+    savefig(plot_pos, "soliton_position.pdf")
+
+    plot_max = plot(legend=:bottomright)
+    plot!(output_times, maxs, label="theory")
+
+    anim = @animate for i ∈ 1:num_snapshots
+        rho = npzread(joinpath(output_dir, "rho_$i.npy"))
+        psi = npzread(joinpath(output_dir, "psi_$i.npy"))
+
+        density_plot = contourf(
+                                rho[:, :, resol÷2],
+                                title=raw"$\rho$",
+                                clims=(0, maximum(maxs)),
+                                showaxis=false,
+                                ticks=false,
+                                aspect_ratio=:equal,
+                               )
+        phase_plot = contourf(
+                              angle.(psi[:, :, resol÷2]),
+                              title="phase",
+                              clims=(-π, +π),
+                              showaxis=false,
+                              ticks=false,
+                              aspect_ratio=:equal,
+                             )
+
+        plot(density_plot, phase_plot, )
+    end
+
+    gif(anim, "soliton_velocity.gif", fps = 15)
 end
 
-gif(anim, "soliton_velocity.gif", fps = 15)
+run_sim()
+
+if MPI.Comm_rank(comm) == 0
+    plot_results()
+end
