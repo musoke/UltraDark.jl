e12d4ae2a935d7d41a30a7b2e34cb34207281673
Nathan Musoke <nathan.musoke@unh.edu>
2021-07-21 17:01:51 -0300

    feat: phase difference options
    Allow user to choose the maximum phase difference and density threshold.

--- src/config.jl
+++ src/config.jl
@@ -6,6 +6,8 @@ struct SimulationConfig
     time_step_update_period::Int64
     "function defining a(t)"
     a
+    phase_grad_limit::Float64
+    density_threshold::Float64
 end
 
 function constant_scale_factor(t)
@@ -13,7 +15,21 @@ function constant_scale_factor(t)
 end
 
 function SimulationConfig(time_step_update_period)
-    SimulationConfig(time_step_update_period, constant_scale_factor)
+    SimulationConfig(time_step_update_period, constant_scale_factor, π/4, 1e-6)
+end
+
+function SimulationConfig(time_step_update_period, a)
+    SimulationConfig(time_step_update_period, a, π/4, 1e-6)
+end
+
+function SimulationConfig(
+                          ;
+                          time_step_update_period=10,
+                          a=constant_scale_factor,
+                          phase_grad_limit=π/4,
+                          density_threshold=1e-6
+                         )
+    SimulationConfig(time_step_update_period, constant_scale_factor, phase_grad_limit, density_threshold)
 end
 
 end # module
