e579ce6462f69aa1e5f9aca53e572e326c3df27a
Nathan Musoke <nathan.musoke@gmail.com>
2021-11-24 19:16:59 -0500

    bench: Add benchmarks with MPI and redo plots
    Add benchmarks of `PencilGrids`, which allows MPI parallelism.  Redo
    plots of time step benchmarks.

--- benchmarks/time_step/slurm.sh
+++ benchmarks/time_step/slurm.sh
@@ -4,12 +4,15 @@
 #SBATCH --time                  00:20:00      # Walltime
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1       # Can be overridden by sbatch args, e.g. in submit.bash
-#SBATCH --mem                   30G
+#SBATCH --mem                   4G
 #SBATCH --output                log/benchmark.%j.out # Include the job ID in the names of
 #SBATCH --error                 log/benchmark.%j.err # the output and error files
 
 echo CLUSTER=$CLUSTER >&2
 echo SLURM_CLUSTER_NAME=$SLURM_CLUSTER_NAME >&2
+echo ULTRADARK_GRIDS_TYPE=$ULTRADARK_GRIDS_TYPE >&2
+echo SLURM_NTASKS=$SLURM_NTASKS, SLURM_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK >&2
+echo RESOL_LIST=$RESOL_LIST >&2
 
 case $CLUSTER in
     "sdf")
@@ -22,9 +25,21 @@ case $CLUSTER in
         ;;
     "plasma")
         echo "Configuring for plasma" >&2
-        module load openmpi
-        module load fftw3
-        SRUN_CMD=""
+        module purge
+        # module load openmpi
+        # module load fftw3
+        module load gcc/8.1.0
+        module load craype-x86-rome
+        module load craype-network-infiniband
+        module load cray-libsci/20.03.1
+        module load craype/2.6.4
+        module load perftools-base/20.03.0
+        module load cce/9.1.3
+        module load PrgEnv-cray/1.0.6
+        module load cray-impi/5
+        module load cray-fftw/3.3.8.5
+        # module load cray-fftw_impi/3.3.8.5
+        SRUN_CMD="mpiexec"
         ;;
     "MAHUIKA")
         echo "Configuring for mahuika" >&2
@@ -33,10 +48,17 @@ case $CLUSTER in
         SRUN_CMD=srun
         ;;
     *)
-        SRUN_CMD=srun
+        echo "Configuring for generic" >&2
+        SRUN_CMD="mpiexec"
 esac
 
-julia --project -e 'using Pkg; Pkg.status()'
+if [ "$ULTRADARK_GRIDS_TYPE" == "Grids" ]; then
+    SRUN_CMD=""
+fi
+
+echo SRUN_CMD=$SRUN_CMD >&2
+
+julia --project -e 'using Pkg; Pkg.status()' >&2
 
 # MPI.jl warns that it is running with default MPI binary on a cluster
 # Docs say to add something like this
@@ -44,17 +66,15 @@ echo "Building MPI.jl" >&2
 julia --project -e 'ENV["JULIA_MPI_BINARY"]="system"; using Pkg; Pkg.build("MPI"; verbose=true)'
 
 
-resol_list="64 128 256 512"
-
 #############
 # UltraDark.jl
 echo "Writing output to $OUTPUT_FILE_UD for UltraDark.jl" >&2
 
-for resol in $resol_list
+for resol in $RESOL_LIST
 do
         export ULTRADARK_RESOL=$resol
         echo $(date) >&2
-        echo "Starting Julia script with threads=$SLURM_CPUS_PER_TASK, resol=$ULTRADARK_RESOL" >&2
+        echo "Starting Julia script with tasks=$SLURM_NTASKS, threads=$SLURM_CPUS_PER_TASK, resol=$ULTRADARK_RESOL" >&2
         $SRUN_CMD julia --project -t $SLURM_CPUS_PER_TASK time_step.jl $resol >> $OUTPUT_FILE_UD
 done
 
@@ -66,7 +86,7 @@ done
 #cd ../PyUltraLight
 
 #echo "Writing output to $OUTPUT_FILE_PUL for PyUltraLight" >&2
-#for resol in $resol_list
+#for resol in $RESOL_LIST
 #do
 #        echo $(date) >&2
 #        echo "Starting PyUltraLight script with threads=$SLURM_CPUS_PER_TASK, resol=$resol" >&2
--- benchmarks/time_step/submit.bash
+++ benchmarks/time_step/submit.bash
@@ -5,7 +5,10 @@ case $(hostname) in
     "sdf-login01")
         export CLUSTER=sdf
         ;;
-    "ln-0001")  # plasma
+    "ln-0001")
+        export CLUSTER=plasma
+        ;;
+    "ln-0002")
         export CLUSTER=plasma
         ;;
     "MAHUIKA")
@@ -19,13 +22,38 @@ esac
 export OUTPUT_FILE_UD=benchmark_output_ud-$CLUSTER.csv
 # export OUTPUT_FILE_PUL=benchmark_output_pul-$(CLUSTER).csv
 
-echo "threads,resol,time" > $OUTPUT_FILE_UD
-# echo "threads,resol,time" > $OUTPUT_FILE_PUL
+echo "tasks,threads,resol,time,grids_type" > $OUTPUT_FILE_UD
+# echo "tasks,threads,resol,time,grids_type" > $OUTPUT_FILE_PUL
 
-export ULTRADARK_GRIDS_TYPE=Grids
+MAX_CPUS_TOTAL=16
+MAX_THREADS=16
+MAX_TASKS=16
+declare -a grids_array=("Grids", "PencilGrids")
 
-for threads in $(seq 1 16)
-do
-    echo "Submitting job with --cups-per-task=$threads"
-    sbatch --cpus-per-task=$threads --export=ALL,OUTPUT_FILE_UD,OUTPUT_FILE_PUL,ULTRADARK_GRIDS_TYPE,CLUSTER slurm.sh
+for nthreads in $(seq 1 $MAX_THREADS); do
+
+    for ntasks in $(seq 1 $MAX_TASKS); do
+
+        for ULTRADARK_GRIDS_TYPE in "Grids" "PencilGrids"; do
+
+            export ULTRADARK_GRIDS_TYPE=$ULTRADARK_GRIDS_TYPE #So slurm job can inherit it
+
+            if [ $nthreads = 1 ] && [ $ntasks = 1 ]; then
+                export RESOL_LIST=$(seq 64 32 256)
+            else
+                export RESOL_LIST="64 128 256"
+            fi
+
+            if [ $ntasks -gt 1 ] && [ $ULTRADARK_GRIDS_TYPE != "PencilGrids" ]; then
+                continue
+            fi
+
+            if [ $MAX_CPUS_TOTAL -lt $((nthreads*ntasks)) ]; then
+                continue
+            fi
+
+            sbatch --cpus-per-task=$nthreads --ntasks=$ntasks --export=ALL,OUTPUT_FILE_UD,OUTPUT_FILE_PUL,ULTRADARK_GRIDS_TYPE,CLUSTER,RESOL_LIST slurm.sh
+            echo "Submitted job with --cpus-per-task=$nthreads --ntasks=$ntasks, ULTRADARK_GRIDS_TYPE=$ULTRADARK_GRIDS_TYPE"
+        done
+    done
 done
