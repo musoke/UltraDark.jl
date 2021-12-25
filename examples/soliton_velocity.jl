a6f52326064b5f70820230dd3d92357bf86f7228
Nathan Musoke <nathan.musoke@gmail.com>
2021-12-24 23:09:10 -0400

    doc: Convert examples from Plots.jl to Makie.jl
    Plots.jl seems to break quite easily, especially in CI and clusters.
    Makie.jl seems not to have this problem.
    
    Fixes https://github.com/musoke/UltraDark.jl/issues/82

--- examples/soliton_velocity.jl
+++ examples/soliton_velocity.jl
@@ -1,6 +1,6 @@
 using MPI
 using NPZ
-using Plots
+using CairoMakie
 using UltraDark
 
 if ~MPI.Initialized()
@@ -22,7 +22,7 @@ const velocity = [1, 0, 0]
 const phase = 0
 const t0 = 0
 
-const output_dir = "output/vel"
+const output_dir = "output/vel/centered_on_soliton"
 
 function run_sim()
 
@@ -51,12 +51,12 @@ function plot_results()
         reshape(position0, (1, :)) .+
         (reshape(velocity, (1, :)) .* reshape(output_times, (:, 1)))
 
-    pos_sim = Array{Float64}(undef, (num_snapshots, 3))
-    maxs = Array{Float64}(undef, num_snapshots)
-
     x = npzread(joinpath(output_dir, "x.npy"))
     y = npzread(joinpath(output_dir, "y.npy"))
     z = npzread(joinpath(output_dir, "z.npy"))
+
+    pos_sim = Array{Float64}(undef, (num_snapshots, 3))
+    maxs = Array{Float64}(undef, num_snapshots)
 
     for i in 1:num_snapshots
         rho = npzread(joinpath(output_dir, "rho_$i.npy"))
@@ -65,51 +65,50 @@ function plot_results()
 
         maxs[i] = rhomax
         pos_sim[i, :] .= [x[maxindex[1]], y[maxindex[2]], z[maxindex[3]]]
-
     end
 
     yerr = abs(x[2] - x[1]) / 2
-    plot_pos = plot(legend = :bottomright)
-    scatter!(
+
+    fig_pos = Figure()
+    ax_pos = Axis(fig_pos[1, 1], ylabel = "position", xlabel = L"t")
+
+    lines!(ax_pos, output_times, pos_the[:, 1], label = "theory")
+    errorbars!(
+        ax_pos,
         output_times,
         pos_sim[:, 1],
-        yerr = yerr,
-        markershape = :auto,
+        yerr,
+        whiskerwidth = 10,
         label = "simulation",
     )
-    plot!(output_times, pos_the[:, 1], label = "theory")
-    plot!(xlabel = raw"$t$", ylabel = raw"$x$")
+    axislegend(ax_pos, position = :rc)
 
-    savefig(plot_pos, "soliton_position.pdf")
+    save("soliton_position.pdf", fig_pos)
 
-    plot_max = plot(legend = :bottomright)
-    plot!(output_times, maxs, label = "theory")
+    fig_max, ax_max, p_max = scatter(
+        output_times,
+        maxs,
+        label = "theory",
+        axis = (limits = (nothing, nothing, 0, maximum(maxs) * 1.1),),
+    )
 
-    anim = @animate for i in 1:num_snapshots
+    save("soliton_max.pdf", fig_max)
+
+    fig_rho = Figure()
+    ax_rho = Axis(fig_rho[1, 1], aspect = DataAspect())
+    ax_psi = Axis(fig_rho[2, 1], aspect = DataAspect())
+    colorrange = (0, maximum(maxs))
+    Colorbar(fig_rho[1, 2], limits = colorrange, label = L"\rho")
+    Colorbar(fig_rho[2, 2], limits = (-π, π), label = L"arg(\psi)")
+
+    record(fig_rho, "soliton_velocity.mp4", 1:num_snapshots) do i
         rho = npzread(joinpath(output_dir, "rho_$i.npy"))
         psi = npzread(joinpath(output_dir, "psi_$i.npy"))
 
-        density_plot = contourf(
-            rho[:, :, resol÷2],
-            title = raw"$\rho$",
-            clims = (0, maximum(maxs)),
-            showaxis = false,
-            ticks = false,
-            aspect_ratio = :equal,
-        )
-        phase_plot = contourf(
-            angle.(psi[:, :, resol÷2]),
-            title = "phase",
-            clims = (-π, +π),
-            showaxis = false,
-            ticks = false,
-            aspect_ratio = :equal,
-        )
+        heatmap!(fig_rho[1, 1], x[:], y[:], rho[:, :, resol÷2])
 
-        plot(density_plot, phase_plot)
+        heatmap!(fig_rho[2, 1], x[:], y[:], angle.(psi[:, :, resol÷2]))
     end
-
-    gif(anim, "soliton_velocity.gif", fps = 15)
 end
 
 run_sim()
