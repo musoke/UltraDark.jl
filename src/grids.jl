musoke/UltraDark.jl

commit 9b17f3037c4bb013c1aa714cf51383391314d193

opt: Use Folds.jl for E_grav
Fixes https://github.com/musoke/UltraDark.jl/issues/144

src/grids.jl
@@ -464,13 +464,22 @@ function mass(grids)
 end
 
 """
+    E_gravity_density(psi, Phi)
+
+Gravitational energy density of field `psi` in gravitational potential `Phi`
+"""
+function E_gravity_density(psi::Number, Phi::Number)
+    Phi .* abs2.(psi) / 2.0
+end
+
+"""
     E_grav(grids)
     E_grav(grids, psi)
 
 Gravitational potential energy
 """
 function E_grav(grids, psi)
-    Folds.sum(grids.Φx .* abs2.(psi)) * dV(grids) / 2.0
+    Folds.mapreduce(E_gravity_density, +, grids.Φx, psi) * dV(grids)
 end
 
 function E_grav(grids::AbstractGrids)
@@ -535,20 +544,20 @@ function angular_momentum_density(grids, ψx, ρx)
 
     momentum .*= reshape(ρx, 1, size(ρx)...)
 
-    angular_momentum = similar(momentum)
+    angular_momentum_density = similar(momentum)
 
-    @inbounds @threads for I in CartesianIndices(angular_momentum)
+    @inbounds @threads for I in CartesianIndices(angular_momentum_density)
         _, i, j, k = Tuple(I)
-        # angular_momentum[:, i, j, k] = cross([grids.x[i], grids.y[j], grids.z[k]], momentum[:, i, j, k])
-        angular_momentum[1, i, j, k] =
+        # angular_momentum_density[:, i, j, k] = cross([grids.x[i], grids.y[j], grids.z[k]], momentum[:, i, j, k])
+        angular_momentum_density[1, i, j, k] =
             y[j] * momentum[3, i, j, k] - z[k] * momentum[2, i, j, k]
-        angular_momentum[2, i, j, k] =
+        angular_momentum_density[2, i, j, k] =
             z[k] * momentum[1, i, j, k] - x[i] * momentum[3, i, j, k]
-        angular_momentum[3, i, j, k] =
+        angular_momentum_density[3, i, j, k] =
             x[i] * momentum[2, i, j, k] - y[j] * momentum[1, i, j, k]
     end
 
-    angular_momentum
+    angular_momentum_density
 end
 
 """

benchmarks/folds.jl
@@ -23,6 +23,14 @@ suite["Base"]["sum"] = @benchmarkable sum($(grids.ρx))
 suite["Folds.jl"]["sum"] = @benchmarkable Folds.sum($(grids.ρx))
 
 
+function product_density(ρx)
+    ρx .* ρx^0.5 / 2
+end
+
+suite["Base"]["sum_product"] = @benchmarkable sum(product_density.($(grids.ρx)))
+suite["Folds.jl"]["sum_product"] =
+    @benchmarkable Folds.mapreduce(product_density, +, $(grids.ρx))
+
 tune!(suite)
 
 results = run(suite)
@@ -30,4 +38,4 @@ results = run(suite)
 @show res_min = minimum(results)
 @show res_med = median(results)
 
-judge(res_med["Folds.jl"], res_med["Base"])
+display(judge(res_med["Folds.jl"], res_med["Base"]))

commit 0f95fb3032dfc0f6b07c371f333afb5d3b15d07b

fix: Typo in gravitational energy
9b17f3037c4bb013c1aa714cf51383391314d193
(https://github.com/musoke/UltraDark.jl/pull/152) introduced a bug in
the gravitational energy.
The arguments of E_gravity_density are reversed.  This doesn't show up in
tests because the psi used is real, so the incorrect energy density can
be coerced to be real.

Fix this and update the tests of summaries so that psi is complex.

Project.toml
@@ -1,7 +1,7 @@
 name = "UltraDark"
 uuid = "1c8d022d-dfc0-4b41-80ab-3fc7e88cdfea"
 authors = ["Nathan Musoke <n.musoke@auckland.ac.nz>"]
-version = "0.9.4"
+version = "0.9.5"
 
 [deps]
 AbstractFFTs = "621f4979-c628-5d54-868e-fcf4e3e8185c"

src/grids.jl
@@ -467,7 +467,7 @@ end
 
 Gravitational energy density of field `psi` in gravitational potential `Phi`
 """
-function E_gravity_density(psi::Number, Phi::Number)
+function E_gravity_density(psi::Number, Phi::Real)::Real
     Phi .* abs2.(psi) / 2.0
 end
 
@@ -478,7 +478,7 @@ end
 Gravitational potential energy
 """
 function E_grav(grids, psi)
-    Folds.mapreduce(E_gravity_density, +, grids.Φx, psi) * dV(grids)
+    Folds.mapreduce(E_gravity_density, +, psi, grids.Φx) * dV(grids)
 end
 
 function E_grav(grids::AbstractGrids)

test/summary.jl
@@ -28,7 +28,7 @@ pencilgrids = PencilGrids(1.0, 8)
 external_states = ()
 
 for g in [grids, pencilgrids]
-    g.ψx .= 1.0
+    g.ψx .= 1.0 + im
     @. g.ρx = abs2(g.ψx)
 end
 