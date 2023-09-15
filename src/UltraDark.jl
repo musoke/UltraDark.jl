c8e71bd6735a806ef2e214cf538c9b9e1776f427
Nathan Musoke <nathan.musoke@unh.edu>
2023-09-14 18:50:51 -0600

    print memory usage after each step

--- src/UltraDark.jl
+++ src/UltraDark.jl
@@ -5,6 +5,8 @@ using AbstractFFTs: fftfreq, rfftfreq
 import Folds
 using LinearAlgebra
 using PencilFFTs, MPI
+using Printf: @printf
+
 
 using FFTW
 
@@ -164,6 +166,10 @@ function take_steps!(grids, t_start, Δt, n, output_config, a, constants, extern
         end
 
         output_summary_row(grids, output_config, t, a(t), Δt, constants, external_states)
+
+        # @show InteractiveUtils.varinfo()
+        meminfo_julia()
+        GC.gc()
     end
 
     outer_step!(Δt / 2, grids, constants; a = a(t))
@@ -271,6 +277,40 @@ function simulate!(
         throw("Phase gradient is too large to end")
     end
 
+end
+
+function meminfo_julia()
+  # @printf "GC total:  %9.3f MiB\n" Base.gc_total_bytes(Base.gc_num())/2^20
+  # Total bytes (above) usually underreports, thus I suggest using live bytes (below)
+  @printf "GC live:   %9.3f MiB\n" Base.gc_live_bytes()/2^20
+  @printf "JIT:       %9.3f MiB\n" Base.jit_total_bytes()/2^20
+  @printf "Max. RSS:  %9.3f MiB\n" Sys.maxrss()/2^20
+end
+
+function meminfo_procfs(pid=getpid())
+  smaps = "/proc/$pid/smaps_rollup"
+  if !isfile(smaps)
+    error("`$smaps` not found. Maybe you are using an OS without procfs support or with an old kernel.")
+  end
+
+  rss = pss = shared = private = 0
+  for line in eachline(smaps)
+    s = split(line)
+    if s[1] == "Rss:"
+      rss += parse(Int64, s[2])
+    elseif s[1] == "Pss:"
+      pss += parse(Int64, s[2])
+    elseif s[1] == "Shared_Clean:" || s[1] == "Shared_Dirty:"
+      shared += parse(Int64, s[2])
+    elseif s[1] == "Private_Clean:" || s[1] == "Private_Dirty:"
+      private += parse(Int64, s[2])
+    end
+  end
+
+  @printf "RSS:       %9.3f MiB\n" rss/2^10
+  @printf "┝ shared:  %9.3f MiB\n" shared/2^10
+  @printf "┕ private: %9.3f MiB\n" private/2^10
+  @printf "PSS:       %9.3f MiB\n" pss/2^10
 end
 
 end # module
